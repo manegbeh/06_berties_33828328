594eca39c18e6d744d5f11a72a3527df3e7d0eb9
manegbeh <150786806+manegbeh@users.noreply.github.com>
2025-11-13 15:07:58 +0000

    reuploaded my files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# 06_berties_33828328
+# Berties Books
 
+Sample code for Dynamic Web Applications module.
+
+Uses node.js with Express and EJS.
+
+To install and run:
+
+npm install
+node index.js
---
+++ routes/books.js
@@ -0,0 +1,53 @@
+// Create a new router
+const express = require("express")
+const router = express.Router()
+
+router.get('/search',function(req, res, next){
+    res.render("search.ejs")
+});
+
+router.get('/search-result', function (req, res, next) {
+     //searching in the database
+    let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.search_text + "%'"; // query database to get all books with name that contains the keyword using "like"
+    console.log(sqlquery)
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("searchresult.ejs", {availableBooks: result})
+    });
+});
+
+router.get('/list', function(req, res, next) {
+    let sqlquery = "SELECT * FROM books"; // query database to get all the books
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("list.ejs", {availableBooks: result})
+    });
+});
+
+router.get('/addbook',function(req, res, next){
+    res.render('addbook.ejs')
+});
+
+router.post('/bookadded', function (req, res, next) {
+    // saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    // execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
+    })
+}) 
+
+
+// Export the router object so index.js can access it
+module.exports = router
