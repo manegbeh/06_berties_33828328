979285f8b3fde7bba52cd9ca157c3dda258cb5bb
manegbeh <150786806+manegbeh@users.noreply.github.com>
2025-11-19 21:13:10 +0000

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -11,16 +11,7 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
--- Create the application user
-CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'zxcvbnm';
-GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
-FLUSH PRIVILEGES;
+# Create the application user
+CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
+GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
 
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) UNIQUE,
-  first VARCHAR(50),
-  last VARCHAR(50),
-  email VARCHAR(100),
-  hashedPassword VARCHAR(255)
-);
