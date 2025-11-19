138dd421341b2f5395758d4e1f46fe7d6b0ee0e1
manegbeh <150786806+manegbeh@users.noreply.github.com>
2025-11-19 21:04:35 +0000

    Update insert_test_data.sql

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, first, last, email, hashedPassword)
+VALUES ('gold', 'Gold', 'User', 'gold@example.com', '$2b$12$Ct2d7EGeqJSFrLZ0oHZqZeiY4W5SgrA8J7ov3wPjY/n82QgWJ9uxG');
