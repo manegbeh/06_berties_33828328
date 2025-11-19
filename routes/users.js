6079d5a1e7793f0291e7d7a4f45638ea2dc3861f
manegbeh <150786806+manegbeh@users.noreply.github.com>
2025-11-19 20:05:59 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 // Create a new router
 const express = require("express")
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const router = express.Router()
 
 router.get('/register', function (req, res, next) {
@@ -7,9 +9,32 @@ router.get('/register', function (req, res, next) {
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-}); 
+
+    const first = req.body.first
+    const last = req.body.last
+    const email = req.body.email
+    const username = req.body.username
+    const plainPassword = req.body.password
+
+    // Hash password
+    bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+        if (err) return next(err)
+
+        // Insert user into database
+        const sqlquery = "INSERT INTO users (username, first, last, email, hashedPassword) VALUES (?,?,?,?,?)"
+
+        db.query(sqlquery, [username, first, last, email, hashedPassword], (err, result) => {
+            if (err) return next(err)
+
+            let output = `Hello ${first} ${last}, you are now registered! `
+            output += `We will send an email to you at ${email}<br><br>`
+            output += `Your password is: ${plainPassword}<br>`
+            output += `Your hashed password is: ${hashedPassword}`
+
+            res.send(output)
+        })
+    })
+})
 
 // Export the router object so index.js can access it
 module.exports = router
